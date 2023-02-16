7519a83ce8f8a051ffd52472b25a8e595857c15e
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-16 12:42:38 +0300

    Firrst attempt, Header.jsx component file completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-//initialization sity
+import React from "react";
+
+function Header(props) {
+    return (
+        <header>
+            <h1>{props.name}</h1>
+        </header>
+    )
+}
+
+export default Header
